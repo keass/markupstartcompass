f4162647facdeaa5083b6b365567c769ba354aa5
dongsoo <keass9@gmail.com>
2015-05-14 18:19:24 +0900

    add mixin : sprite, border-radius add config : img path changer, output, linecomments

--- config.rb
+++ config.rb
@@ -7,15 +7,18 @@ css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
+# http_generated_images_path = "img";
+# http_generated_images_path = "http://static.adress.com/img/";
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
